819df28310ce6ae90e3c26843aac991ea08a6177
ghost-aramic <owinoojala@gmail.com>
2023-11-17 09:40:43 +0300

    chore: Explore supertest

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,41 @@
 import { describe, test, expect } from '@jest/globals'
+import express, {Application, NextFunction, Request, Response} from "express"
+import request from "supertest"
+const app: Application = express()
 
-describe('Sum Module', () =>{
-    test('Adds up to 10', () =>{
-        const sum = 2 + 8
+app.get('/user', (req: Request, res: Response, next: NextFunction) =>{
+    const user = {
+        name: 'John',
+        age: 30,
+        height: '5\" 7\''
+    }
+    res.status(200).json({ user })
+})
 
-        expect(sum).toEqual(10)
+describe('GET User', () =>{
+    test('Responds with json', () =>{
+        request(app).get('/user')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+            if (err) throw err
+        })
+    })
+
+    test('Responds with 200 status code', () =>{
+        request(app).get('/user')
+            .expect(200)
+            .end(function(err, res){
+            if (err) throw err
+        })
+    })
+
+    test('Response data has properties name, age and height', async() =>{
+        const response = await request(app).get('/user')
+        
+        expect(response.body.user).toHaveProperty('name')
+        expect(response.body.user).toHaveProperty('age')
+        expect(response.body.user).toHaveProperty('height')
     })
 })
+
